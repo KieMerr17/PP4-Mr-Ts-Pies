2dc3a3a56e1444b00e1c1e8f0175e3feab0f2eab
KieMerr17 <kieran.merrett@hotmail.com>
2023-05-26 11:58:24 +0000

    add summernote filter and search fields to admin panel

--- workshops/admin.py
+++ workshops/admin.py
@@ -6,4 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Workshop)
 class WorkshopAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'event_date', 'created_on')
+    list_display = ('title', 'slug', 'chef', 'event_date', 'created_on')
+    search_fields = ('title', 'content')
     summernote_fields = ('content')
