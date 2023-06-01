8442f9ad44906602cc28fcab0286d21376086472
KieMerr17 <kieran.merrett@hotmail.com>
2023-06-01 15:17:58 +0000

    remove models from database and build in base.html template

--- workshops/views.py
+++ workshops/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Workshop
 
-# Create your views here.
+
+class WorkshopList(generic.ListView):
+    model = Workshop
+    queryset = Workshop.objects.filter(status=1).order_by('-created_on')
+    template_name = 'workshops.html'
+    paginate_by = 4
+
