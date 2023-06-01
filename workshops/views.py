03572830951ec05e2c143f9568055c1ce274b1af
KieMerr17 <kieran.merrett@hotmail.com>
2023-06-01 17:01:12 +0000

    add placeholder image, adjust workshop information display properties and paginate

--- workshops/views.py
+++ workshops/views.py
@@ -5,7 +5,7 @@ from .models import Workshop
 
 class WorkshopList(generic.ListView):
     model = Workshop
-    queryset = Workshop.objects.filter(status=1).order_by('-created_on')
+    queryset = Workshop.objects.filter(status=1).order_by('event_date')
     template_name = 'workshops.html'
     paginate_by = 4
 
