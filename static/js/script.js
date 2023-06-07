637d85c2085d533b2fceb61b510040df97897251
KieMerr17 <kieran.merrett@hotmail.com>
2023-06-07 22:14:13 +0000

    add scroll opacity to background image in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const marketItems = document.getElementById('markets');
+const marketScrollWidth = marketItems.scrollWidth;
+
+$(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    var maxOpacity = 0.8; // maximum opacity value
+    var minOpacity = 0.0; // minimum opacity value
+
+    var opacity = maxOpacity - (scrollTop / ($(window).height() * 2)); // opacity based on the user's position
+    
+    if (opacity < minOpacity) { // ensure the opacity does not go below the minimum value
+      opacity = minOpacity;
+    }
+    
+    $('.overlay').css('background-color', 'rgba(0, 0, 0, ' + opacity + ')'); // update the background color opacity
+});
