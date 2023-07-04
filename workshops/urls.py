0c6348f79130c47c305c8614c26a7f9c844e41d6
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-04 21:00:43 +0000

    create base view, template and link url for workshop details pages

--- workshops/urls.py
+++ workshops/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.WorkshopList.as_view(), name='workshops')
+    path('', views.WorkshopList.as_view(), name='workshops'),
+    path('<slug:slug>/', views.WorkshopDetail.as_view(), name='workshop_detail'),
 ]
