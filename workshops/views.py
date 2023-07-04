9b998bdb854c417d3396061a2efcf29df32c529c
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-04 19:01:04 +0000

    add workshop detail view to workshop/views.py

--- workshops/views.py
+++ workshops/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Workshop
 
 
@@ -9,3 +9,22 @@ class WorkshopList(generic.ListView):
     template_name = 'workshops.html'
     paginate_by = 4
 
+
+class WorkshopDetail(View):
+    
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Workshop.objects.filter (status=1)
+        workshop = get_object_or_404(queryset, slug=slug)
+        liked = False
+
+        if workshop.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "workshop_detail.html",
+            {
+                "workshop": workshop,
+                "liked": liked
+            }
+        )
