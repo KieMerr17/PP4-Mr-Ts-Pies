dee0c553fbafdd7a5c30ca90bde4c06ccdbebe03
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-10 20:38:51 +0000

    create the admin panel for news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Article)
+class NewsAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on', 'author')
+    list_display = ('title', 'slug', 'author', 'created_on')
+    search_fields = ('title', 'content')
+    summernote_fields = ('content')
+
