dc09e0d964740657d1d5cbf74c4eaf94cfc1d8d7
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-12 18:12:46 +0000

    create and connect the article_detail pages

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
@@ -8,3 +8,23 @@ class ArticleList(generic.ListView):
     queryset = Article.objects.filter(status=1).order_by('created_on')
     template_name = 'news.html'
     paginate_by = 3
+
+
+class ArticleDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter (status=1)
+        article = get_object_or_404(queryset, slug=slug)
+        liked = False
+
+        if article.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "news_detail.html",
+            {
+                "article": article,
+                "liked": liked
+            }
+        )
