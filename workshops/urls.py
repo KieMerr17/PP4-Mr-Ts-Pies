4c2bd932fa2d046363ad51d5683b160ad1b89ae5
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-23 09:36:29 +0000

    add the booking urls

--- workshops/urls.py
+++ workshops/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.WorkshopList.as_view(), name='workshops'),
     path('<slug:slug>/', views.WorkshopDetail.as_view(), name='workshop_detail'),
+    path('bookings/', login_required(views.BookingListView.as_view()), name='booking_list'),
+    path('bookings/create/', login_required(views.BookingCreateView.as_view()), name='booking_create'),
+    path('bookings/<int:pk>/', login_required(views.BookingDetailView.as_view()), name='booking_detail'),
 ]
