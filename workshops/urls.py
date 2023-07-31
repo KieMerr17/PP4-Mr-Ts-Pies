e06f56e73988c34b5438f78eadf0aee19dca4a09
KieMerr17 <kieran.merrett@hotmail.com>
2023-07-31 20:47:49 +0000

    add the url for the booking form

--- workshops/urls.py
+++ workshops/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.WorkshopList.as_view(), name='workshops'),
     path('<slug:slug>/', views.WorkshopDetail.as_view(), name='workshop_detail'),
+    path('workshop/<int:workshop_id>/book/', views.book_workshop, name='book_workshop'),
 ]
