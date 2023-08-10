1392d1980a2dfc9ce5248fa3805e5ff8f0eae361
KieMerr17 <kieran.merrett@hotmail.com>
2023-08-10 21:32:31 +0000

    create file alerts.js for fade out js actions across multiple pages

---
+++ static/js/alerts.js
@@ -0,0 +1,8 @@
+/**Fade out animation for sign in/out success notifications on index.html */ 
+const alertMessages = document.querySelectorAll(".alert-success");
+setTimeout(function() {
+  alertMessages.forEach(function(alertMessages) {
+    alertMessages.style.animation = "fadeout 1s forwards";
+  });
+  // set fade out to 2 seconds
+}, 2000);
--- static/js/script.js
+++ static/js/script.js
@@ -18,11 +18,3 @@ $(window).scroll(function() {
 });
 
 
-/**Fade out animation for sign in/out success notifications on index.html */ 
-const alertMessages = document.querySelectorAll(".alert-success");
-setTimeout(function() {
-  alertMessages.forEach(function(alertMessages) {
-    alertMessages.style.animation = "fadeout 1s forwards";
-  });
-  // set fade out to 1.5 seconds
-}, 1500);
