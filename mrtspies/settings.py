930e2e02d88b8e8a231b702650ce7d4f8e09014b
KieMerr17 <kieran.merrett@hotmail.com>
2023-08-11 19:48:08 +0000

    update local host port

--- mrtspies/settings.py
+++ mrtspies/settings.py
@@ -32,9 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['mrtspies.herokuapp.com',
                  'localhost',
-                 '8000-kiemerr17-pp4mrtspies-9cy47l899b0.ws-eu102.gitpod.io']
+                 '8000-kiemerr17-pp4mrtspies-9cy47l899b0.ws-eu103.gitpod.io']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-kiemerr17-pp4mrtspies-9cy47l899b0.ws-eu102.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-kiemerr17-pp4mrtspies-9cy47l899b0.ws-eu103.gitpod.io']
 
 
 # Application definition
