29b7e428a5c17db001ca5f805d6637a0d5ad0ede
KieMerr17 <kieran.merrett@hotmail.com>
2023-08-16 19:35:07 +0000

    add basic comments section to news_detail template and adjust to work

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.ArticleList.as_view(), name='news'),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='news_detail'),
-    path('article/<slug:slug>/add_comment/', views.add_article_comment, name='article_comment_add'),
+    path('article/<slug:slug>/add_comment/', views.ArticleComment, name='article_comment_add'),
 ]
