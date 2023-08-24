24bc5a237692860cce5cc340a7cdf2c655835ff8
KieMerr17 <kieran.merrett@hotmail.com>
2023-08-24 21:26:43 +0000

    add url pattern for delete comment in news_detail

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.ArticleList.as_view(), name='news'),
     path('<slug:slug>/', views.ArticleDetail.as_view(), name='news_detail'),
     path('article/<slug:slug>/add_comment/', views.ArticleComment, name='article_comment_add'),
+    path('delete_comment/<int:comment_id>/<slug:slug>/', views.delete_comment, name='delete_comment'),
 ]
