20fb5c2990d1e12c8d5a014277e0673b457384d4
KieMerr17 <kieran.merrett@hotmail.com>
2023-09-01 10:49:34 +0000

    create pies model and register it in the admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Pie, Allergy
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Pie)
+class PiesAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('allergies', 'diet')
+    list_display = ('title', 'diet')
+    search_fields = ('title', 'allergies', 'diet')
+    summernote_fields = ('description',)
+
+# @admin.register(Allergy)
+# class AllergyAdmin(admin.ModelAdmin):
+#     list_display = ('allergy',)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+ALLERGIES = ((0, "Milk"), (1, "Egg"), (2, "Fish"), (3, "Nuts"), (4, "Soy"))
+DIET = ((0, "Meat"), (1, "Vegetarian"), (2, "Pescetarian"), (3, "Vegan"), (4, "Nut Allergy"))
+
+
+class Allergy(models.Model):
+    allergy = models.IntegerField(choices=ALLERGIES, default=0)
+
+    def __str__(self):
+        return ALLERGIES[self.allergy][1]
+
+
+class Pie(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    diet = models.IntegerField(choices=DIET, default=0)
+    allergies = models.ManyToManyField(Allergy, blank=True)
+
+    def __str__(self):
+        return self.title
