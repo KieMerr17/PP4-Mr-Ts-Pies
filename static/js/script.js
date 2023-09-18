30dbb12058e792ab1ea603b9b6452352ca6c5a61
KieMerr17 <kieran.merrett@hotmail.com>
2023-09-18 08:55:05 +0000

    revert function to hide comments

--- static/js/script.js
+++ static/js/script.js
@@ -15,18 +15,3 @@ $(window).scroll(function() {
     
     $('.overlay').css('background-color', 'rgba(0, 0, 0, ' + opacity + ')'); // update the background color opacity
 });
-
-
-/** Function to display and hide the comments in the news detail */
-
-document.getElementById('toggle-comments').addEventListener('click', function () {
-  var commentsBox = document.getElementById('comments-box');
-
-  if (commentsBox.style.display === 'none') {
-      commentsBox.style.display = 'block';
-      this.innerText = 'Hide Comments';
-  } else {
-      commentsBox.style.display = 'none';
-      this.innerText = 'View Comments';
-  }
-});
