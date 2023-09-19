449dc5cd64d3400a68fd2c4cb9535a4e8cd7c8d0
KieMerr17 <kieran.merrett@hotmail.com>
2023-09-19 11:16:54 +0000

    build in like function for logged in users to the pie cards

--- menu/urls.py
+++ menu/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PieList.as_view(), name='pies'),
+    path('like/<slug:slug>', views.PieLike.as_view(), name='pie_like'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic, View
 from .models import Pie
+
 
 
 class PieList(generic.ListView):
     model = Pie
     template_name = 'pies.html'
+
+
+class PieLike(View):
+
+    def post(self, request, slug):
+        pie = get_object_or_404(Pie, slug=slug)
+        if pie.likes.filter(id=request.user.id).exists():
+            pie.likes.remove(request.user)
+        else:
+            pie.likes.add(request.user)
+
+        return redirect('pies')
