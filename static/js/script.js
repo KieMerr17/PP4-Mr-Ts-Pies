e28c86ce51b1de5452d2d9b159c94d0b257c28bd
KieMerr17 <kieran.merrett@hotmail.com>
2023-09-26 21:29:30 +0000

    adjust script.js file for validation and upload validation images

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,26 @@
 /**Background Opacity Scrolling function
  * Adjust the opacity shadowed over the background image in index.html */ 
-const marketItems = document.getElementById('markets');
 
-$(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    var maxOpacity = 0.9; // maximum opacity value
-    var minOpacity = 0.4; // minimum opacity value
+function OverlayOpacity() {
+  var scrollTop = window.scrollY || document.documentElement.scrollTop; // Get the current scroll position
+  var maxOpacity = 0.9; // Maximum opacity value
+  var minOpacity = 0.4; // Minimum opacity value
 
-    var opacity = maxOpacity - (scrollTop / ($(window).height() * 2)); // opacity based on the user's position
-    
-    if (opacity < minOpacity) { // ensure the opacity does not go below the minimum value
-      opacity = minOpacity;
-    }
-    
-    $('.overlay').css('background-color', 'rgba(0, 0, 0, ' + opacity + ')'); // update the background color opacity
-});
+  // Calculate opacity based on the user's position
+  var opacity = maxOpacity - (scrollTop / (window.innerHeight * 2));
+
+  // Ensure the opacity does not go below the minimum value
+  opacity = Math.max(opacity, minOpacity);
+
+  // Update the background color with the class 'overlay'
+  var overlayElements = document.querySelectorAll('.overlay');
+  for (var i = 0; i < overlayElements.length; i++) {
+      overlayElements[i].style.backgroundColor = 'rgba(0, 0, 0, ' + opacity + ')';
+  }
+}
+
+// Attach the overlay function to the scroll event
+window.addEventListener('scroll', OverlayOpacity);
+
+// Call the function
+OverlayOpacity();
