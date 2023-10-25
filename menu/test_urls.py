42fe839ee202813094570a9beed7020936b3c493
KieMerr17 <kieran.merrett@hotmail.com>
2023-10-25 16:42:04 +0000

    add automated tests for menu/urls.py

---
+++ menu/test_urls.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import PieList, PieLike
+
+# These tests check that the pies url is correct and that the link once liking
+# a pie is also correct
+
+class TestUrls(SimpleTestCase):
+    def test_pie_list_url_resolves(self):
+        url = reverse('pies')
+        self.assertEquals(resolve(url).func.view_class, PieList)
+
+    def test_pie_like_url_resolves(self):
+        url = reverse('pie_like', args=['test-pie'])
+        self.assertEquals(resolve(url).func.view_class, PieLike)
