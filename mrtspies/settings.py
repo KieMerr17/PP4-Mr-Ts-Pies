8b488da0a3fc16e527781e46d2c66078c70a42ed
KieMerr17 <kieran.merrett@hotmail.com>
2023-11-24 22:49:21 +0000

    adjust allowed host and debug settings

--- mrtspies/settings.py
+++ mrtspies/settings.py
@@ -28,12 +28,17 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['mrtspies.herokuapp.com',
-                 'www.mrtspies.com',
+                 'https://www.mrtspies.com',
+                 'https://www.mrtspies.co.uk',
+                 'https://mrtspies.com',
+                 'https://mrtspies.co.uk',
+                 'mrtspies.com',
+                 'mrtspies.co.uk',
                  'localhost',
                  '8000-kiemerr17-pp4mrtspies-9cy47l899b0.ws-eu105.gitpod.io']
 
